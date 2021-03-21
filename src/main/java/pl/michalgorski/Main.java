a68619d4c7c19280c56f6dba5d3fc904ad410103
MichalG11 <grsk11@wp.pl>
2021-03-21 22:01:17 +0100

    first 3

--- src/main/java/pl/michalgorski/Main.java
+++ src/main/java/pl/michalgorski/Main.java
@@ -7,9 +7,9 @@ public class Main {
         //poczatek projektu zrobiony na masterze
 
         //robimy releasa_20190329 - release zrobiony lokalnie
-
-
-
+    
+        
+        
         //zmiany Marcina domergowane do releasa
         //zmiany Rafala domergowane do releasa
 
